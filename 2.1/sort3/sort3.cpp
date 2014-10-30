ca55f3d50b4d9ed9b1f025c7956979d9c0815529
haozy <450522366@qq.com>
2014-10-30 02:47:15 -0400

    fractions && sort3 init.

---
+++ 2.1/sort3/sort3.cpp
@@ -0,0 +1,20 @@
+/*
+ID:haoziyi1
+PROG:sort3
+LANG:C++
+*/
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+using namespace std;
+ifstream fin("sort3.in");
+ofstream fout("sort3.out");
+int main(){
+
+}
+
