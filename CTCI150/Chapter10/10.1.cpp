d35827accea9fe91c788b248ce57decadb0eccf7
haozy <450522366@qq.com>
2014-11-04 02:01:11 -0500

    Starting Chapter10

---
+++ CTCI150/Chapter10/10.1.cpp
@@ -0,0 +1,22 @@
+/*
+Auther:haozy
+Problem:10.1
+Language:C++
+Description:service called by 1000 client to get end-of-day stock price information
+*/
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include"../struct.h"
+using namespace std;
+
+
+int main(){
+
+}
+
