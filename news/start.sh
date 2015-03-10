447023bbbda1b9b7724eb3e4dbe727f8506d93b8
Hao <ziyihao@ud43d7ec95f6854ab676f.ant.amazon.com>
2015-03-10 18:14:59 +0800

    use sed

--- news/start.sh
+++ news/start.sh
@@ -3,7 +3,7 @@ rooturl="http://news.cnblogs.com"
 url="/"
 wget $rooturl$url -O "index.html2"
 echo "after wget"
-grep -A 2 "news_entry" "index.html2"|./removeH2|./ParseA > "finalTitle"
+grep -A 2 "news_entry" "index.html2"|sed -n 2~4p|./ParseA > "finalTitle"
 echo "Done"
 
 
